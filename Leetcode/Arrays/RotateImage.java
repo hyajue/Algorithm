668823e9494411e02122f3ed5b745254af740a43
Aaron Liu <zlsuperdan@gmail.com>
2017-02-22 02:36:35 -0800

    updates 2/22

---
+++ Leetcode/Arrays/RotateImage.java
@@ -0,0 +1,49 @@
+/**
+* You are given an n x n 2D matrix representing an image.
+* 
+* Rotate the image by 90 degrees (clockwise).
+* 
+* Follow up:
+* Could you do this in-place?
+*/
+
+/*
+solution 1:
+step 1: transpose the matrix: matrix[i][j] <-> matrix[j][i]
+step 2: flip each row of transpsed matrix
+*/
+
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+		if (matrix == null || matrix.length == 0) {
+			return;
+		}
+		int rowNum = matrix.length;
+		int colNum = rowNum;
+		// transpose the matrix
+		for (int i = 0; i < rowNum; i++) {
+			for (int j = i + 1; j < colNum; j++) {
+				swap(i, j, matrix);
+			}
+		}
+		// flip each row of transposed matrix
+		for (int i = 0; i < rowNum; i++) {
+			for (int j = 0; j < (colNum >> 1); j++) {
+				flip(i, j, matrix);
+			}
+		}
+    }
+	private void swap(int i, int j, int[][] matrix) {
+		int tmp = 0;
+		tmp = matrix[i][j];
+		matrix[i][j] = matrix[j][i];
+		matrix[j][i] = tmp;
+	}
+	private void flip(int i, int j, int[][] matrix) {
+		int colNum = matrix[0].length;
+		int tmp = 0;
+		tmp = matrix[i][j];
+		matrix[i][j] = matrix[i][colNum-j-1];
+		matrix[i][colNum-j-1] = tmp;
+	}
+} 
