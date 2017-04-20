aeae158cc8fee756e1ac7540c01ab1fefe00aad5
Aaron Liu <zlsuperdan@gmail.com>
2017-04-20 09:21:00 -0700

    updates 4/20/2017

---
+++ Leetcode/Sort/FirstMissingPositive.java
@@ -0,0 +1,24 @@
+/**
+* Given an unsorted integer array, find the first missing positive integer.
+* 
+* For example,
+* Given [1,2,0] return 3,
+* and [3,4,-1,1] return 2.
+* 
+* Your algorithm should run in O(n) time and uses constant space.
+*/
+
+/*
+复杂度
+时间：O(n) 空间O(1)
+
+思路：类似index counting sort 
+
+*/
+
+ 
+public class FirstMissingPositive {
+	public int firstMissingPositive(int[] nums) {
+	  
+	}
+}
