766bcf320e59afe6555c53cf2b7fc7b24a6589a3
Aaron Liu <zlsuperdan@gmail.com>
2017-04-27 13:55:24 -0700

    Update MergeIntervals.java

--- Leetcode/Sort/MergeIntervals.java
+++ Leetcode/Sort/MergeIntervals.java
@@ -25,6 +25,21 @@ return [1,6],[8,10],[15,18].
  */
 public class MergeIntervals {
     public List<Interval> merge(List<Interval> intervals) {
-        
+        List<Interval> res = new ArrayList<Interval>();
+        if (intervals == null || intervals.size() == 0) return res;
+        Comparator<Interval> cmp = new Comparator<Interval>() {
+            @Override
+            Public int compare(Interval i1, Interval i2) {
+                if (i1.start == i2.start) {
+                  return i1.end - i2.end;    
+                } 
+                return i1.end - i2.end;
+            }
+        };
+        Collections.sort(intervals, cmp);
+        res.add(intervals.get(0));
+        for (int i = 1; i < intervals.size(); i++) {
+            int 
+        }
     }
 }
