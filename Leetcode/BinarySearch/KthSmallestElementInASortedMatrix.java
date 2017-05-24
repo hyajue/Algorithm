c7bf72aa209bb0e0d7d1545657d8e33da575fcb0
Aaron Liu <zlsuperdan@gmail.com>
2017-05-23 23:47:54 -0700

    updates 5/23/2017

--- Leetcode/BinarySearch/KthSmallestElementInASortedMatrix.java
+++ Leetcode/BinarySearch/KthSmallestElementInASortedMatrix.java
@@ -17,31 +17,79 @@
 * You may assume k is always valid, 1 ≤ k ≤ n2.
 */
 
+/*
+solution 1
+复杂度
+时间：O((n*m)*logk) 空间：O(k)
 
+思路：最大堆
+用maxheap把全部元素放进heap里面，同时如果heap的size大于k就弹出，保持heap的size为k，
+最后root的元素就是第k小
+*/
+
+public class KthSmallestElementInASortedMatrix {
+  public int kthSmallest(int[][] matrix, int k) {
+    // heap
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k + 1, (a, b) -> b - a);
+    for(int i = 0; i < matrix.length; i++) {
+      for(int j = 0; j < matrix[0].length; j++) {
+        maxHeap.offer(matrix[i][j]);
+        if(maxHeap.size() > k) maxHeap.poll();
+      }
+    }
+    return maxHeap.poll();
+  }
+}
+
+/*
+solution 2
+复杂度
+时间：O((n*m)*logk) 空间：O(k)
+
+思路：二分
+
+*/
 
 public class KthSmallestElementInASortedMatrix {
   public int kthSmallest(int[][] matrix, int k) {
     int row = matrix.length;
     int left = matrix[0][0];
     int right = matrix[row-1][row-1];
-    while (left <= right) {
+    int[] idx = new int[2];
+		
+		while (left <= right) {
 			int mid = left + (right - left) / 2;
-			int num = cnt(matrix, mid);
-		}		
+			int num = cnt(matrix, mid, idx);
+			if (num == k) {
+				return matrix[idx[0]][idx[1]];
+			} else if (num < k) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+			idx[0] = 0;
+			idx[1] = 0;
+		}
+    return 0;		
   }
 	
-	private int cnt(int[][] matrix, int target) {
+	private int cnt(int[][] matrix, int target, int[] idx) {
 		int row = matrix.length;
 		int res = 0;
 		int i = row - 1;
 		int j = 0;
+		boolean foundIdx = false;
 		while (i >= 0 && j < row) {
-			if (matrix[i][j] < target) {
+			if (matrix[i][j] <= target) {
 				res += i + 1;
 				j++;
 			} else {
 				i--;
-			} 
+			}
+      if (j > res[1]) {
+				idx[0] = i;
+				idx[1] = j - 1;
+			}			
 		}
 		return res;
 	}
