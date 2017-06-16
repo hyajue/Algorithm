02c8a7ebf3b48a7371227d2b00bb71dec0c8d3a0
AaronLiuAL <zlsuperdan@gmail.com>
2017-06-15 18:07:39 -0700

    updates 6/15/2017

--- Leetcode/Arrays/RangeAddition.java
+++ Leetcode/Arrays/RangeAddition.java
@@ -37,28 +37,28 @@
 /*
 跟算法无关，这是个数学题
 思想是把所有需要相加的值存在第一个数，然后把这个范围的最后一位的下一位(end+1)减去这个inc, 
-这样我所以这个范围在求最终值的时候，都可以加上这个inc，而后面的数(end位之后的位)就不会加上inc
+这样在这个范围在求最终值的时候，都可以加上这个inc，而后面的数(end位之后的位)就不会加上inc
 */
 
 public class RangeAddition {
   public int[] getModifiedArray(int length, int[][] updates) {
     if (length < 0) return null;
-	int[] res = new int[length];
-	for (int i = 0; i < updates.length; i++) {
-	  int start = updates[i][0];
-	  int end = updates[i][1];
-	  int inc = updates[i][2];
-	  res[start] += inc;
-	  if (end < length - 1) {
-		res[end+1] -= inc;
+	  int[] res = new int[length];
+	  for (int i = 0; i < updates.length; i++) {
+	    int start = updates[i][0];
+	    int end = updates[i][1];
+	    int inc = updates[i][2];
+	    res[start] += inc;
+	    if (end < length - 1) {
+	  	  res[end+1] -= inc;
+	    }
 	  }
-	}
-	int sum = 0;
-	for (int i = 0; i < length; i++){
-	  sum += res[i];
-	  res[i] = sum;
-	}
-	return res;
+	  int sum = 0;
+	  for (int i = 0; i < length; i++){
+	    sum += res[i];
+	    res[i] = sum;
+	  }
+	  return res;
   } 
 }
 
