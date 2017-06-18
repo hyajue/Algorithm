26fd08ee1f154828be59b6cc24e5c13d725dacd4
Aaron Liu <zlsuperdan@gmail.com>
2017-06-18 00:48:19 -0700

    updates 6/17/2017

--- Leetcode/TwoPointers/3SumSmaller.java
+++ Leetcode/TwoPointers/3SumSmaller.java
@@ -11,28 +11,38 @@
 * Could you solve it in O(n2) runtime?
 */
 
- 
+/*
+复杂度
+时间：O(n^2) 空间：O(1)
+
+思路：三指针
+维护三个指针i, j, k,先对输入数组排序,方便跳过重复元素
+对k -> [0, len-3]:
+  i = k + 1， j = len - 1; 
+  i,j 双指针相对而行, 如果i，j，k指向的数相加小于target,则j取[i+1, j]之间的所有组合都满足要求,
+  因为j最大,最大的可以,比它小的也肯定满足可以.更新cnt后直接把i指针右移即可.   
+*/ 
 
 public class ThreeSumSmaller {
   public int threeSumSmaller(int[] nums, int target) {
     if (nums == null || nums.length == 0) return 0;
-	Arrays.sort(nums); 
-	int len = nums.length;
-	int cnt = 0;
+	  Arrays.sort(nums); 
+	  int len = nums.length;
+	  int cnt = 0;
 	
-	for (int k = 0; k < len-2; k++) {
-	  int i = k + 1;
+	  for (int k = 0; k < len-2; k++) {
+	    int i = k + 1;
       int j = len - 1;
       while (i < j) {
-		int sum = nums[k] + nums[i] + nums[j];
-		if (sum >= target) {
-		  j--;
-		} else {
-		    cnt += j - i;
-			i++;
-		}
-	  }	  
-	}
-	return cnt;
+	  	  int sum = nums[k] + nums[i] + nums[j];
+	  	  if (sum >= target) {
+	  	    j--;
+	  	  } else {
+	  	    cnt += j - i;
+	  		  i++;
+	  	  }
+	    }	  
+	  }
+	  return cnt;
   }
 }
