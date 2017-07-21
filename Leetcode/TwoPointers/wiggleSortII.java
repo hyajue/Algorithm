a9bf87d1da4e7a5607420cbb062cd4b9f9c51c72
Aaron Liu <zlsuperdan@gmail.com>
2017-07-21 00:07:19 -0700

    updates 7/21/2017

--- Leetcode/TwoPointers/wiggleSortII.java
+++ Leetcode/TwoPointers/wiggleSortII.java
@@ -9,7 +9,18 @@
 * Given nums = [1, 3, 2, 2, 3, 1], one possible answer is [2, 3, 1, 3, 1, 2].
 */
 
+/*
+复杂度
+时间：O(n) 空间：O(1)
 
+思路：三路快排
+直观想法是找到数组的中位数，然后根据中位数对数组划分为两部分。大的那一部分放在索引为奇数的位置(1，3，5，7，9…)，
+小的那一部分放在索引为偶数的位置(0，2，4，6，8…)，可以随意放 
+想法对，但还不够.比如当数组为[4,5,5,6]时，得到的结果就是[4,5,5,6]，这不符合题目要求，正确的结果应该是[5,6,4,5]
+这里问题出在两部分数中相同大小的数放在一起了。 
+为了避免这个问题，直观上来说，需要把较小的那一部分中比较大的数和较大的那一部分中比较小的数放的越远越好
+这里,三路快排的思想就起作用了.
+*/
 
 public class wiggleSortII {
   public void wiggleSort(int[] nums) {
@@ -50,14 +61,27 @@ public class wiggleSortII {
 		
     private void rearrange(int[] nums, int lo, int hi, int mid) {
       int i = lo;
-	    int j = hi + 1;
 	    while(i <= hi) {
-	      if (nums[mapping(i)] > mid)  
-	    
+	      if (nums[mapping(i)] > mid) {
+					swap(nums, mapping(i), mapping(lo));
+					i++;
+					lo++;
+				} else if (nums[mapping(i)] < mid) {
+					swap(nums, mapping(i), mapping(hi));
+					hi--;
+				} else {
+					i++;
+				}      
 		  }
     }
 			
     private int mapping(int index) {
 	    return (2*index+1) % (nums.length | 1);
     }
+		
+		private void swap(int[] nums, int i, int j) {
+			int tmp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = tmp;
+		}
 }
