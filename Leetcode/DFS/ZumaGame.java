911c2f8bbcc81d093d62f535bbcb4e388f09dd5f
Aaron Liu <zlsuperdan@gmail.com>
2017-07-27 09:38:49 -0700

    updates 7/26/2017

--- Leetcode/DFS/ZumaGame.java
+++ Leetcode/DFS/ZumaGame.java
@@ -30,3 +30,83 @@
 * Both input strings will be non-empty and only contain characters 'R','Y','B','G','W'.
 */
 
+
+public class ZumaGame {
+  public int findMinStep(String board, String hand) {
+    int res = -1;
+    Map<Character, Integer> map = new HashMap<Character, Integer>();
+		List<Character> list = new ArrayList<Character>();
+    
+		for (int i = 0; i < hand.length(); i++) {
+			if (!map.containsKey(hand.charAt(i))) {
+				map.put(hand.charAt(i), 1);
+			} else {
+				map.put(hand.charAt(i), map.get(hand.charAt(i))+1);
+			}
+		} 
+    for (int i = 0; i < board.length(); i++) {
+			list.add(board.charAt(i));
+		}
+		
+		res = helper(list, map);
+    return res;		
+  }
+
+  private int helper(List<Character> list, Map<Character, Integer> map) {
+		removeTriple(list);
+		if (list.size() == 0) return 0;
+		if (isEmptyHand(map)) return -1;
+		int cnt = 0;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < list.size(); i++) {
+			char cur = list.get(i);
+			cnt++;
+			if (i == list.size()-1 || list.get(i+1) != cur) {
+				int toGet = 3 - cnt;
+				if (map.containsKey(cur) && map.get(cur) >= toGet) {
+					map.put(cur, map.get(cur) - toGet);
+					List<Character> newList = new ArrayList<Character>(list);
+					for (int j = 0; j < cnt; j++) {
+						newList.remove(i-j);
+					}
+					int subRes = helper(newList, map);
+					if (subRes != -1) {
+						min = Math.min(subRes+toGet, min);
+					}
+					map.put(cur, map.get(cur)+toGet);
+				}
+				cnt = 0;
+			}
+		}
+		return (min == Integer.MAX_VALUE) ? -1 : min;
+	}	
+	
+	private void removeTriple(List<Character> list) {
+		int cnt = 0;
+		boolean removed = false;
+		for (int i = 0; i < list.size(); i++) {
+			char cur = list.get(i);
+			cnt++;
+			if (i == list.size()-1 || list.get(i+1) != cur) {
+				if (cnt >= 3) {
+					for (int j = 0; j < cnt; j++) {
+						list.remove(i-j);
+					}
+					removed = true;
+					break;
+				}
+				cnt = 0;
+			}
+		}
+		if (removed) {
+			removeTriple(list);
+		}
+	}
+	
+	private boolean isEmptyHand(Map<Character, Integer> map) {
+		for (int val : map.values()) {
+			if (val > 0) return false;
+		}
+		return true;
+	}
+}
